dd3878ceb1f99be94ed582ecb8633324eeb7b2c9
ammar-muslim <newamaremara@gmail.com>
2025-05-26 01:46:05 +0300

    fix: revert to Next.js built-in CSS handling

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ['www.noor-book.com', 'www.mojml.com'],
   },
-  webpack: (config, { isServer }) => {
-    // Add proper CSS configuration
-    config.module.rules.push({
-      test: /\.(css|scss|sass)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader',
-        'sass-loader',
-      ],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
