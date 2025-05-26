720c5a0693b129650741133edcc377940a081a15
ammar-muslim <newamaremara@gmail.com>
2025-05-26 03:00:39 +0300

    fix: update page props types in article page

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import ArticleContent from "./components/ArticleContent";
 import { getData } from "./server";
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { slug: string } }) {
   const article = await getData(params.slug);
   return {
     title: article.title,
@@ -10,7 +10,13 @@ export async function generateMetadata({ params }: { params: { slug: string } })
   };
 }
 
-export default async function ArticlePage({ params }: { params: { slug: string } }) {
+export default async function ArticlePage({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   const article = await getData(params.slug);
   if (!article) {
     return <div>Article not found</div>;
